648658dec641f0cfc678984e2a66c4241496c168
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-06-28 23:24:46 +0530

    updated code

--- src/components/Currency.vue
+++ src/components/Currency.vue
@@ -2,17 +2,15 @@
   <div class="hello">
     <h1>{{ title }}</h1>
     <select name="base" v-model="base">
-      <option v-for="currency in currency_list">
-        {{ currency }}
+      <option v-bind:value="currency" v-for="currency in currency_list">
+        {{ currency.name }}
       </option>
     </select>
-    <select name="to" v-model="to">
-      <option v-for="currency in currency_list">
-        {{ currency }}
+    <select name="target" v-model="target">
+      <option v-bind:value="currency" v-for="currency in currency_list">
+        {{ currency.name }}
       </option>
     </select>
-    <input v-on:keypress="onPriceChange" name="base_amount" v-model="base_amount" />
-    <input v-on:keypress="onPriceChange" name="to_amount" v-model="to_amount" />
   </div>
 </template>
 
@@ -20,13 +18,31 @@
 import axios from 'axios';
 const fixerUrl = 'http://api.fixer.io/latest'
 
-function convert (base, to) {
-  axios.get(fixerUrl, {
+function getRates (base) {
+  return axios.get(fixerUrl, {
     params: {
       base: base
-      symbols: `${base},${to}`
     }
-  }).then(res => conz)
+  }).then(res => {
+
+    const {data} = res;
+    var currency_list = [];
+
+    currency_list.push({
+      name: data.base,
+      value: 1
+    });
+
+    for(var rate in data.rates) {
+      currency_list.push({
+        name: rate,
+        value: data.rates[rate]
+      });
+    }
+
+    return currency_list
+  
+  })
 }
 
 export default {
@@ -36,33 +52,23 @@ export default {
       title: 'Currency Converter',
       currency_list: [],
       base: '',
-      to: '',
+      target: '',
       base_amount: 0,
-      to_amount: 0
+      target_amount: 0
     }
   },
   created: function () {
 
-    axios.get(fixerUrl)
-      .then((res) => {
-
-        this.currency_list.push(res.data.base);
-
-        for(var rate in res.data.rates) {
-          this.currency_list.push(rate);
-        }
-
-        this.base = this.currency_list[0];
-        this.to = this.currency_list[1];
-
+    getRates('USD')
+      .then(res => {
+        this.currency_list = res;
+        this.base = res[0];
+        this.target = res[1];
       })
-  },  
-  methods: {
-    onPriceChange: function (e) {
-      
-
-
-    }
+    
+  },
+  updated: function () {
+    console.log(this.base)
   }
 }
 
